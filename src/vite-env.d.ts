58c73d19fbe65f6eb261d063c378d5d906084467
steoiro <stellaoiro11@gmail.com>
2025-05-16 20:00:50 +0300

    chore: add configurations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_RPC_URL: string;
+  readonly VITE_BASE_CHAIN_ID: string;
+  readonly VITE_LOAN_CONTRACT_ADDRESS: string;
+  readonly VITE_USDC_CONTRACT_ADDRESS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface Window {
+  ethereum?: any;
+}
