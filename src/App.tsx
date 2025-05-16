b14744cde6a128a12d267ef32277d1c7fcd1b678
steoiro <stellaoiro11@gmail.com>
2025-05-16 19:59:41 +0300

    feat: add useWebsocket to the App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import DashboardPage from './pages/DashboardPage';
@@ -13,26 +13,29 @@ import RegisterPage from './pages/RegisterPage';
 import LandingPage from './pages/LandingPage';
 import Layout from './components/layout/Layout';
 import { UserProvider } from './contexts/UserContext';
+import { Web3Provider } from './contexts/Web3Context';
 
 function App() {
   return (
     <ThemeProvider>
-      <UserProvider>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/app" element={<Layout />}>
-            <Route index element={<DashboardPage />} />
-            <Route path="apply" element={<LoanApplicationPage />} />
-            <Route path="loans" element={<LoanManagementPage />} />
-            <Route path="coolers" element={<CoolerMonitoringPage />} />
-            <Route path="impact" element={<ImpactMetricsPage />} />
-            <Route path="marketplace" element={<MarketplacePage />} />
-            <Route path="profile" element={<ProfilePage />} />
-          </Route>
-        </Routes>
-      </UserProvider>
+      <Web3Provider>
+        <UserProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/app" element={<Layout />}>
+              <Route index element={<DashboardPage />} />
+              <Route path="apply" element={<LoanApplicationPage />} />
+              <Route path="loans" element={<LoanManagementPage />} />
+              <Route path="coolers" element={<CoolerMonitoringPage />} />
+              <Route path="impact" element={<ImpactMetricsPage />} />
+              <Route path="marketplace" element={<MarketplacePage />} />
+              <Route path="profile" element={<ProfilePage />} />
+            </Route>
+          </Routes>
+        </UserProvider>
+      </Web3Provider>
     </ThemeProvider>
   );
 }
